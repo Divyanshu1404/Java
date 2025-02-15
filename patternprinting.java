472ee3c5c56069ac42546586d2e486c454f9e8ba
DIVYANSHU MISHRA <145105300+Divyanshu1404@users.noreply.github.com>
2025-02-15 14:28:36 +0530

    Add files via upload

---
+++ patternprinting.java
@@ -0,0 +1,16 @@
+package javabasics;
+
+public class patternprinting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i , j;
+		for(i=1;i<=5;i++) {
+			for(j=1;j<=4;j++) {
+				System.out.print(" * ");
+			}
+			System.out.println("*");
+		}
+	}
+
+}
